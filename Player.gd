4b694a18539cc162c4a971c591c80079db0c6a3a
Maxtheking123 <47282020+Maxtheking123@users.noreply.github.com>
2024-08-28 19:56:00 +0200

    now ice, more bug fix., better jump

--- Player.gd
+++ Player.gd
@@ -1,9 +1,12 @@
 extends KinematicBody2D
 
 # Configurable properties
-var gravity = 1500
+var gravity = 1000
+var minGravity = 500
+var maxGravity = 2000
 var runSpeed = 300
-var jumpForce = 500
+var jumpForce = 450  # Maximum initial jump force
+var maxJumpHoldTime = 0.4  # Maximum time the player can hold the jump button to increase jump height
 var wallSlideSpeed = 200
 var defaultWallSlideFriction = 0.1
 var wallFrictionMap = {}  # Dictionary to hold friction values for different wall types
@@ -16,6 +19,9 @@ var isSliding = false
 var wallJumpAvailable = true
 var wallJumpTimer = 0.0
 var wallJumpGraceTimer = 0.0
+var isJumping = false  # Track if the player is currently in a jump
+var jumpHoldTimer = 0.0  # Track how long the jump button has been held
+var canJump = true  # Used for jump state control
 
 # Nodes
 onready var animatedSprite = $AnimatedSprite
@@ -31,7 +37,7 @@ func _physics_process(delta):
 	
 	var wallFriction = get_wall_friction()
 
-	if isOnWall and isHoldingWall and wallJumpGraceTimer <= 0:
+	if isOnWall and isHoldingWall and not isOnFloor and wallJumpGraceTimer <= 0:
 		handle_wall_slide(wallFriction)
 	elif not isOnFloor:
 		handle_fall(delta)
@@ -39,7 +45,7 @@ func _physics_process(delta):
 		handle_ground()
 
 	handle_horizontal_movement()
-	handle_jump(isOnFloor, isOnWall)
+	handle_jump(isOnFloor, isOnWall, delta)
 	
 	move_and_slide(playerVelocity, Vector2.UP)
 	update_animation(isOnFloor, isOnWall, isSliding)
@@ -63,14 +69,15 @@ func handle_wall_slide(wallFriction):
 	playerVelocity.y = lerp(playerVelocity.y, wallSlideSpeed, wallFriction)
 
 func handle_fall(delta):
-	var gravityModifier = max(-(playerVelocity.y - 190) / 20000 + 0.3, 0.0001)
-	playerVelocity.y += gravity * delta * gravityModifier
+	playerVelocity.y += gravity * delta
 	isSliding = false
 
 func handle_ground():
 	playerVelocity.y = 5
 	isSliding = false
 	wallJumpAvailable = true
+	isJumping = false  # Reset the jumping state when on the ground
+	canJump = true  # Allow the player to jump again when on the ground
 
 func handle_horizontal_movement():
 	if Input.is_action_pressed("ui_left"):
@@ -82,12 +89,19 @@ func handle_horizontal_movement():
 	else:
 		playerVelocity.x = 0
 
-func handle_jump(isOnFloor: bool, isOnWall: bool):
-	if Input.is_action_pressed("ui_up"):
-		if isOnFloor:
+func handle_jump(isOnFloor: bool, isOnWall: bool, delta: float):
+	if Input.is_action_just_pressed("ui_up"):
+		if canJump:
+			canJump = false  # Prevent multiple jumps until grounded or other conditions met
+			isJumping = true
+			jumpHoldTimer = 0.0
 			playerVelocity.y = -jumpForce
-		elif isOnWall and wallJumpAvailable and wallJumpTimer <= 0 and wallJumpGraceTimer <= 0:
-			playerVelocity.y = -jumpForce
+		print("is wall ", isOnWall)
+		if isOnWall:
+			if wallJumpAvailable:
+				print("wallJumped")
+				playerVelocity.y = -jumpForce
+				
 			# Apply horizontal force away from the wall
 			playerVelocity.x = (runSpeed * (1 if not animatedSprite.flip_h else -1)) * 1.5
 			wallJumpAvailable = false
@@ -95,14 +109,27 @@ func handle_jump(isOnFloor: bool, isOnWall: bool):
 			wallJumpTimer = wallJumpCooldown
 			wallJumpGraceTimer = wallJumpGracePeriod
 
+	if isJumping:
+		if Input.is_action_pressed("ui_up") and jumpHoldTimer < maxJumpHoldTime:
+			jumpHoldTimer += delta
+			# Reduce gravity while holding the jump button to extend the jump duration
+			gravity = minGravity
+		elif Input.is_action_pressed("ui_down"):
+			gravity = maxGravity
+		else:
+			isJumping = false
+			gravity = 1000  # Reset gravity after jump hold time ends
+
 func update_animation(isOnFloor: bool, isOnWall: bool, isSliding: bool):
 	var anim = "idle"
 	if isSliding:
 		anim = "slide"
 	elif not isOnFloor:
 		anim = "fall"
-	elif playerVelocity.x != 0:
+	elif playerVelocity.x != 0 and not isOnWall:
 		anim = "run"
 	if animatedSprite.animation != anim:
 		animatedSprite.stop()
 		animatedSprite.play(anim)
+
+
